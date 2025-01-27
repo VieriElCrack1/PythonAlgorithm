9b6538f9df53a9c870c27cc6adfb249e9657c45b
VieriElCrack1 <santosvieri9@gmail.com>
2025-01-26 22:07:55 -0500

    terminado ejercicio 1

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,1 +1,26 @@
 #https://www.python.org/
+
+"""
+COMENTARIO 2
+"""
+
+'''
+COMENTARIO 4
+'''
+
+prueba_string = "SIUU" #String
+prueba_int = 123 #int | Integer
+prueba_float = 29.9 #double | decimal
+prueba_boolean = True #Boolean
+
+lista = [1, 2, 3, 4, 5] # Colección ordenada de elementos
+
+tupla = (1, 2, 3) # las tuplas no se pueden modificar.
+
+conjunto = {1, 2, 3, 4, 5} # No permite elementos duplicados
+
+diccionario = {"Nombre" : "Vieri", "Año" : 2006} # Cada elemento tiene una clave y un valor
+
+lenguage = "Python"
+print(f"¡Hola, {lenguage}!")
+
